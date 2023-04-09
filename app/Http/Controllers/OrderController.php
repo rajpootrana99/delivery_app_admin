8c767db9bc84e7bb72ba9cf546d935c4d83aad10
Muhammad Rana <muhammadrana523@gmail.com>
2023-04-09 14:20:12 +0500

    Changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -58,10 +58,9 @@ class OrderController extends Controller
                 'order_id' => $result->id,
                 'delivery_point' => $delivery,
             ];
-            DeliveryPoint::create($save_data[$i]);
             $i++;
         }
-        // DeliveryPoint::insert($save_data);
+        DeliveryPoint::insert($save_data);
 
         $message = __('message.update_form', ['form' => __('message.order')]);
         if ($result->wasRecentlyCreated) {
