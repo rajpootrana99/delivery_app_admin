08c51656f101079f8f5ccc229493a5e22db7caaa
Muhammad Rana <muhammadrana523@gmail.com>
2023-04-09 13:32:32 +0500

    Changes

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -14,7 +14,7 @@ class OrderController extends Controller
 {
     public function getList(Request $request)
     {
-        $order = Order::myOrder();
+        $order = Order::with('deliveryPoints')->myOrder();
 
         if ($request->has('status') && isset($request->status)) {
             if (request('status') == 'trashed') {
